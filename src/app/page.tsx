983333d076d3c1ed8c59aef0e37d04d924834ead
HanuCh@udhary <137854084+hanuchaudhary@users.noreply.github.com>
2025-01-09 21:16:51 +0530

    template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,80 @@
-import Image from "next/image";
+import Socials from "@/components/Socials";
+import React from "react";
+import { Skills } from "./Data/data";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const infoArray = [
+    "🍻 | Available for freelance work.",
+    "I'm a full-stack engineer from India.",
+    "Worked with few startups, learn more",
+    "Make videos about frontend on YouTube",
+    "Write technical blogs on Medium",
+    "Let's talk how we can work together!",
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="max-h-screen bg-neutral-900 text-white">
+      <div className="max-w-3xl h-full mx-auto p-8">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl tracking-tight font-semibold">
+            Hi, I'm Kush Chaudhary.
+          </h1>
+          <Socials />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="info">
+          <ul>
+            {infoArray.map((info, index) => (
+              <li key={index} className="text-sm">
+                {info}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="projects mt-4">
+          <h1 className="text-lg">Projects</h1>
+          <ul>
+            {projects.map((project) => (
+              <li key={project.id} className="text-sm flex">
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+                <a href={project.liveLink} target="_blank" rel="noreferrer">
+                  Live
+                </a>
+                <a href={project.github} target="_blank" rel="noreferrer">
+                  Github
+                </a>
+                <p>{project.users} users</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="opensourse mt-5">
+          <h1>Open Source Contributions</h1>
+          <ul>
+            {openSourceContributions.map((contribution) => (
+              <li key={contribution.id} className="flex items-center gap-2">
+                <img src={contribution.image} alt="" />
+                <h1>{contribution.title}</h1>
+                <p>{contribution.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="mt-4">
+          <h1>Skills</h1>
+          <ul className="flex flex-wrap">
+            {Skills.map((skill) => (
+              <li key={skill.name} className="flex items-center gap-2 text-sm">
+                {/* <img src={`/${skill.icon}.svg`} alt="" /> */}
+                <span>{skill.icon}</span>
+                <h1>{skill.name}</h1>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Socials.tsx
@@ -0,0 +1,13 @@
+import { Linkedin } from "lucide-react";
+import React from "react";
+
+export default function Socials() {
+  return (
+    <div className="flex items-center justify-center space-x-4">
+      <img src="/github.svg" alt="" />
+      <img src="/github.svg" alt="" />
+      <img src="/github.svg" alt="" />
+      <h1>kushchaudharyog@gmail.com</h1>
+    </div>
+  );
+}
